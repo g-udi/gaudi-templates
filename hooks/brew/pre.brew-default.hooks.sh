97e6d8014dd983b14603cba59d0c21de1af911c4
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-04-04 02:43:51 +0100

    Update pre.brew-default.hooks.sh

--- hooks/brew/pre.brew-default.hooks.sh
+++ hooks/brew/pre.brew-default.hooks.sh
@@ -4,11 +4,6 @@
 
 echo ""
 
-if [[ $(ls -ld /usr/local | awk '{print $3}') != $(whoami) ]]; then
-    echo "Making sure brew permissions are set up properly 🔑"
-    sudo chown -R $(whoami) $(brew --prefix)/*
-fi
-
 # Check if we have any taps that we need to "tap" ;)
 find "$SOURCE_LOCATION" -type f -iname "default.tap.list.sh" | while read -r TAP; do
     . "$TAP"
