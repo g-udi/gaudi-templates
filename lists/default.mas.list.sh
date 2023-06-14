fbadd3c7226397996334d89651b39111b6ea1ac6
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-06-14 12:15:46 +0100

    remove monity from mas

--- lists/default.mas.list.sh
+++ lists/default.mas.list.sh
@@ -4,7 +4,6 @@ export masList=(
 
     ## Utilities
 
-    "915542151::Monity::Monity is an advanced System Monitoring Widget for OS X. Monity takes it's place in OS X new Today View and oversees your components statistics just for you https://www.monityapp.com/"
     "937984704::Amphetamine::Amphetamine is a little app that lives in your menu bar. With Amphetamine, you can effortlessly override your energy saver settings and keep your Mac awake https://itunes.apple.com/gb/app/amphetamine/id937984704"
     "1354318707::Core Tunnel::It is the missing tunnel manager, compatible with OpenSSH, automatic and intuitive"
     "639968404::Parcel::All Deliveries in One Place. Parcel will tell you that and will inform when your delivery status changes https://parcelapp.net/"
