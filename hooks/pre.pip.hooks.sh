b03eb81ff7766a3f059b81ea5ad8884041c894d7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-06-22 16:00:23 +0100

    Delete pre.pip.hooks.sh

--- hooks/pre.pip.hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck shell=bash
-
-echo "Checking if pip is installed 🤔"
-if ! type pip &> /dev/null ; then
-    printf "The required ${YELLOW}pip${NC} command was not found .. attempting to install it\n"
-    easy_install pip
-fi
